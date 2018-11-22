a1667c8aa753ffeb33f5dc858b3c61cb4e52ca8e
Knox <knox.h@qq.com>
2018-11-22 16:10:42 +0800

    adapter project path

--- utils/package.js
+++ utils/package.js
@@ -3,7 +3,13 @@
 const Fs = require('fire-fs');
 const Path = require('path');
 
-let PATH = Path.join(Editor.Project.path, './assets/resources/i18n');
+// adapter project path
+let projectPath = Editor.projectPath;
+if (!projectPath) {
+   projectPath = Editor.Project.path;
+}
+
+let PATH = Path.join(projectPath, './assets/resources/i18n');
 
 let mount = function () {
     // 创建目录，保证目录存在
