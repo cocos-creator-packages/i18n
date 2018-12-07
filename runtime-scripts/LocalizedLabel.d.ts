fc7b2f9536aa6232ec3aa0ba2b916d5608818bc9
Unknown <2478557459@qq.com>
2018-12-07 11:46:16 +0800

    ts

---
+++ runtime-scripts/LocalizedLabel.d.ts
@@ -0,0 +1,3 @@
+export declare class LocalizedLabel {
+	dataID: string;
+}
---
+++ runtime-scripts/LocalizedSprite.d.ts
@@ -0,0 +1,8 @@
+export declare class LocalizedSprite {
+	spriteFrameSet: SriteFrameSet[];
+}
+
+declare interface SriteFrameSet {
+	language: string;
+	spriteFrame: cc.SpriteFrame;
+}
